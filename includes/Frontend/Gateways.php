97d3ad6103468e0591144a5259a4f8ad48745899
Istiaq Nirab <40653908+Ok9xNirab@users.noreply.github.com>
2023-03-12 07:49:35 +0600

    Fix methods not displaying inside checkout pay page

--- includes/Frontend/Gateways.php
+++ includes/Frontend/Gateways.php
@@ -6,61 +6,67 @@ use SpringDevs\PreOrder\Illuminate\Helper;
 
 /**
  * Class Gateways
+ *
  * @package SpringDevs\PreOrder\Frontend
  */
 
-class Gateways
-{
-    public function __construct()
-    {
-        add_filter('woocommerce_available_payment_gateways', [$this, "filter_available_gateways"], 10, 1);
-        add_filter('woocommerce_gateway_title', [$this, 'change_method_title'], 10, 2);
-        add_filter('woocommerce_gateway_description', [$this, "change_method_description"], 10, 2);
-    }
+class Gateways {
 
-    /**
-     * @param $available_gateways
-     * @return array
-     */
-    public function filter_available_gateways($available_gateways)
-    {
-        if (is_admin() || WC()->cart->get_cart_contents_count() == 0)
-            return $available_gateways;
+	public function __construct() {
+		add_filter( 'woocommerce_available_payment_gateways', array( $this, 'filter_available_gateways' ), 10, 1 );
+		add_filter( 'woocommerce_gateway_title', array( $this, 'change_method_title' ), 10, 2 );
+		add_filter( 'woocommerce_gateway_description', array( $this, 'change_method_description' ), 10, 2 );
+	}
 
-        if (!isset($available_gateways['sdevs-preorder-gateway'])) return [];
+	/**
+	 * Filter Gateways.
+	 *
+	 * @param array $available_gateways Gateways.
+	 * @return array
+	 */
+	public function filter_available_gateways( $available_gateways ) {
+		if ( is_admin() || WC()->cart->get_cart_contents_count() == 0 || is_checkout_pay_page() ) {
+			return $available_gateways;
+		}
 
-        $preorder = false;
+		if ( ! isset( $available_gateways['sdevs-preorder-gateway'] ) ) {
+			return array();
+		}
 
-        foreach (WC()->cart->get_cart() as $cart_item_key => $cart_item) {
-            $preorder = Helper::has_preorder($cart_item['product_id']);
-        }
+		$preorder = false;
 
-        if ($preorder) {
-            $preorder_gateway = $available_gateways['sdevs-preorder-gateway'];
-            $available_gateways = [];
-            $available_gateways['sdevs-preorder-gateway'] = $preorder_gateway;
-        } else {
-            unset($available_gateways['sdevs-preorder-gateway']);
-        }
+		foreach ( WC()->cart->get_cart() as $cart_item_key => $cart_item ) {
+			$preorder = Helper::has_preorder( $cart_item['product_id'] );
+		}
 
-        return $available_gateways;
-    }
+		if ( $preorder ) {
+			$preorder_gateway                             = $available_gateways['sdevs-preorder-gateway'];
+			$available_gateways                           = array();
+			$available_gateways['sdevs-preorder-gateway'] = $preorder_gateway;
+		} else {
+			unset( $available_gateways['sdevs-preorder-gateway'] );
+		}
 
-    public function change_method_title($method_title, $method)
-    {
-        if ($method == 'sdevs-preorder-gateway') {
-            $settings = get_option("woocommerce_sdevs-preorder-gateway_settings");
-            if ($settings) $method_title = __($settings['title'], "sdevs_preorder");
-        }
-        return $method_title;
-    }
+		return $available_gateways;
+	}
 
-    public function change_method_description($description, $method)
-    {
-        if ($method == 'sdevs-preorder-gateway') {
-            $settings = get_option("woocommerce_sdevs-preorder-gateway_settings");
-            if ($settings) $description = __($settings['description'], "sdevs_preorder");
-        }
-        return $description;
-    }
+	public function change_method_title( $method_title, $method ) {
+		if ( $method == 'sdevs-preorder-gateway' ) {
+			$settings = get_option( 'woocommerce_sdevs-preorder-gateway_settings' );
+			if ( $settings ) {
+				$method_title = __( $settings['title'], 'sdevs_preorder' );
+			}
+		}
+		return $method_title;
+	}
+
+	public function change_method_description( $description, $method ) {
+		if ( $method == 'sdevs-preorder-gateway' ) {
+			$settings = get_option( 'woocommerce_sdevs-preorder-gateway_settings' );
+			if ( $settings ) {
+				$description = __( $settings['description'], 'sdevs_preorder' );
+			}
+		}
+		return $description;
+	}
 }
